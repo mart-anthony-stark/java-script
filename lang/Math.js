2997f8dddf8a54e231585175a9087279f280018b
Mart Anthony Salazar <martsalazar0@gmail.com>
2023-02-12 11:31:33 +0800

    Initial commit

---
+++ lang/Math.js
@@ -0,0 +1,44 @@
+module.exports = {
+  sqrt(num) {
+    return Math.sqrt(num);
+  },
+  max(x, y) {
+    return Math.max(x, y);
+  },
+  pow(x, y) {
+    return Math.pow(x, y);
+  },
+  log(x) {
+    return Math.log1p(x);
+  },
+  exp(x) {
+    return Math.exp(x);
+  },
+  expm1(x) {
+    return Math.expm1(x);
+  },
+  sin(a) {
+    return Math.sin(a);
+  },
+  cos(a) {
+    return Math.cos(a);
+  },
+  asin(a) {
+    return Math.asin(a);
+  },
+  acos(a) {
+    return Math.acos(a);
+  },
+  atan(a) {
+    return Math.atan(a);
+  },
+  sinh(a) {
+    return Math.sinh(a);
+  },
+  cosh(a) {
+    return Math.cosh(a);
+  },
+  tanh(a) {
+    return Math.tanh(a);
+  },
+};
